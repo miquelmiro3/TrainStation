860ae65c34a277222b99bb5ff5bc4257494bde4f
Samuel <samuel.alarcon@est.fib.upc.edu>
2020-03-19 17:16:52 +0100

    Adds interaction with objects

---
+++ Assets/Scripts/ChangeColor.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColor : MonoBehaviour
+{
+
+	private Material unselected_material;
+	public Material selected_material;
+
+	public void MakeUnselected() {
+		GetComponent<MeshRenderer>().material=unselected_material;
+	}
+
+	public void MakeSelected() {
+		GetComponent<MeshRenderer>().material=selected_material;
+	}
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		unselected_material = GetComponent<MeshRenderer>().material;
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+		
+    }
+}
