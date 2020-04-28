411fa13dca62e5a94dce9f6ba4484e0466eaed17
Samuel <samuel.alarcon@est.fib.upc.edu>
2020-04-28 12:13:02 +0200

    Improves the ticket line behaviour

--- Assets/Scripts/TicketMachine.cs
+++ Assets/Scripts/TicketMachine.cs
@@ -11,6 +11,9 @@ public class TicketMachine : Interactuable
 	public bool printing;
 	public GameObject lastTicket;
 	private bool ticketForPlayer;
+	public float distanceForPersonInLine;
+	private bool machineInUse;
+	private float smallCooldownForUse;
 
 	private void PrintTicket() {
 		Debug.Log("Giving a ticket");
@@ -28,11 +31,14 @@ public class TicketMachine : Interactuable
 	}
 
 	public override void Interact() {
-		if (!printing) {
-			MakeUninteractuable();
-			printing=true;
-			timeForTicket=cooldownForTickets;
-			ticketForPlayer=true;
+		if (machineInUse) Debug.Log("It's not your turn");
+		else {
+			if (!printing) {
+				MakeUninteractuable();
+				printing=true;
+				timeForTicket=cooldownForTickets;
+				ticketForPlayer=true;
+			}
 		}
 	}
 
@@ -42,15 +48,27 @@ public class TicketMachine : Interactuable
 		timeForTicket=0.0f;
 		cooldownForTickets=3.0f;
 		printing=false;
+		machineInUse=false;
+		smallCooldownForUse=0;
 	}
 
-	private void Update() {
+	override protected void Update() {
+		base.Update();
 		if (printing) {
 			timeForTicket-=Time.deltaTime;
 			if (timeForTicket<=0.0f) {
 				PrintTicket();
 				printing=false;
 			}
+		}
+		RaycastHit hit;
+		Debug.DrawRay(transform.position, transform.TransformDirection(new Vector3(0,1,0))*distanceForPersonInLine, Color.cyan);
+		if (Physics.Raycast(transform.position, transform.TransformDirection(new Vector3(0, 1, 0)), out hit, distanceForPersonInLine, LayerMask.GetMask("Person"))) {
+			machineInUse=true;
+			smallCooldownForUse=1.5f;
+		} else {
+			if (smallCooldownForUse<=0.0f) machineInUse=false;
+			else smallCooldownForUse-=Time.deltaTime;
 		}
 	}
 }
