2a393085cf920e9074a29164c9503510c0a5d12d
Samuel <samuel.alarcon@est.fib.upc.edu>
2020-05-14 14:13:06 +0200

    Structure for the first animators

---
+++ Assets/Scripts/Animation Behaviours/IdleInterruption.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IdleInterruption : StateMachineBehaviour {
+
+	private float timer;
+
+	public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+		timer=Random.Range(8.0f, 16.0f);
+	}
+
+	public override void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+		if (timer>0.0f) {
+			timer-=Time.deltaTime;
+		} else {
+			animator.SetTrigger("IdleInterruption");
+		}
+	}
+}
