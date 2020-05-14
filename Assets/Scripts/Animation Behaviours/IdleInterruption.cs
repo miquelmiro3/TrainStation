ed9152981462c0e98b76e7f3994bae36fb6ab428
Samuel <samuel.alarcon@est.fib.upc.edu>
2020-05-14 17:22:51 +0200

    Finalizes the starting version of the Idle Animation Controller

--- Assets/Scripts/Animation Behaviours/IdleInterruption.cs
+++ Assets/Scripts/Animation Behaviours/IdleInterruption.cs
@@ -5,16 +5,27 @@ using UnityEngine;
 public class IdleInterruption : StateMachineBehaviour {
 
 	private float timer;
+	private bool done;
+	private bool active;
 
 	public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
 		timer=Random.Range(8.0f, 16.0f);
+		done=false;
+		active=true;
+	}
+
+	public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+		active=false;
 	}
 
 	public override void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
-		if (timer>0.0f) {
-			timer-=Time.deltaTime;
-		} else {
-			animator.SetTrigger("IdleInterruption");
+		if (!done && active) {
+			if (timer>0.0f) {
+				timer-=Time.deltaTime;
+			} else {
+				done=true;
+				animator.SetTrigger("IdleInterruption");
+			}
 		}
 	}
 }
