b31321c29ebb43b008ac650712a71ef5ed644a3e
Sergi Avila Sanguesa <sergi.avila.sanguesa@est.fib.upc.edu>
2020-07-13 12:06:32 +0000

    Introduces XR Ray in VR

--- Assets/Scripts/ClickManager.cs
+++ Assets/Scripts/ClickManager.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class ClickManager : MonoBehaviour
 {
-
 	public float distance_for_interactions; // Indicates the maximum distance to allow an interaction with an object
+	public bool vr_mode;
 	private LayerMask l_mask;
 	private GameObject last_selected;
 
@@ -17,32 +17,39 @@ public class ClickManager : MonoBehaviour
 		Debug.Log("RightClick");
 	}
 
+	public void SelectObject(GameObject go) {
+		go.GetComponent<Interactuable>().MakeSelected();
+		if (last_selected) last_selected.GetComponent<Interactuable>().MakeUnselected();
+		last_selected=go;
+	}
+
+	public void UnselectObject() {
+		if (last_selected) {
+			last_selected.GetComponent<Interactuable>().MakeUnselected();
+			last_selected=null;
+		}
+	}
+
 	// Checks if the user is watching an Interactuable object. If they are, call the object's MakeSelected() function. Also calls MakeUnselected() of the previous looked at object
 	private void CheckWhatWeWatch() {
 		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition); // TODO: comprobar que funciona con VR, donde quizas no existe un "mousePosition"
 		RaycastHit hit;
 		if (Physics.Raycast(ray, out hit, distance_for_interactions, l_mask)) {
 			GameObject selected = hit.transform.gameObject;
-			if (selected!=last_selected) {
-				selected.GetComponent<Interactuable>().MakeSelected();
-				if (last_selected) last_selected.GetComponent<Interactuable>().MakeUnselected();
-				last_selected=selected;
-			}
-		} else if (last_selected) {
-			last_selected.GetComponent<Interactuable>().MakeUnselected();
-			last_selected=null;
-		}
+			if (selected!=last_selected) SelectObject(selected);
+		} else UnselectObject();
 	}
 
 	// Start is called before the first frame update
 	void Start()
     {
         l_mask = LayerMask.GetMask("Interactuable");
+		Debug.Log(gameObject.name);
 	}
 
     // Update is called once per frame
     void Update()
     {
-		CheckWhatWeWatch();
+		if (!vr_mode) CheckWhatWeWatch();
 	}
 }
