605653e275c9f36245690ce630832906e9bbfc1a
Samuel <samuel.alarcon@est.fib.upc.edu>
2020-07-15 13:57:22 +0200

    Introduction to NavMesh

---
+++ Assets/Scripts/NavMeshNavigator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavMeshNavigator : MonoBehaviour
+{
+
+	private NavMeshAgent agent;
+	public NavMeshPoints targets;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		agent=GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (agent.remainingDistance<=0.2) {
+			Vector3 destination = targets.GetRandomPoint();
+			agent.SetDestination(destination);
+		}
+    }
+}
