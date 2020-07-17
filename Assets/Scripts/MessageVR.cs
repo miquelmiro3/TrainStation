9019c5d51e86c5d888c658897951b2ab6dabe8c1
Sergi Avila Sanguesa <sergi.avila.sanguesa@est.fib.upc.edu>
2020-07-17 11:28:56 +0000

    Adds VR messages in the UI

---
+++ Assets/Scripts/MessageVR.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MessageVR : MonoBehaviour
+{
+
+	public float time;
+	private float timer;
+	public string message;
+	public bool printing;
+	private Text t;
+
+	public void ShowMessage(string m) {
+		message=m;
+		t.text=message;
+		timer=time;
+		foreach (Transform child in transform) {
+			child.gameObject.active=true;
+		}
+		printing=true;
+	}
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		t=GetComponentInChildren<Text>();
+		timer=time;
+		printing=true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (printing) {
+			timer-=Time.deltaTime;
+			if (timer<=0) {
+				printing=false;
+				foreach (Transform child in transform) {
+					child.gameObject.active=false;
+				}
+			}
+		}
+    }
+}
