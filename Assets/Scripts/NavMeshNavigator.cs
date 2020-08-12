00970fd23c3a9c9126de698092624501ca2348cd
Samuel <samuel.alarcon@est.fib.upc.edu>
2020-08-12 11:29:37 +0200

    Improves the AI, making it harder for the characters to get stuck

--- Assets/Scripts/NavMeshNavigator.cs
+++ Assets/Scripts/NavMeshNavigator.cs
@@ -10,6 +10,15 @@ public class NavMeshNavigator : MonoBehaviour
 	private NavMeshAgent agent;
 	public NavMeshPoints targets;
 	public bool destroyWhenOnDestination;
+	private float timeToConsiderStuck;
+	private float stuckDistance;
+	private float stuckTimer;
+	private Vector3 stuckPosition;
+	private bool panicking;
+
+	public void ActivatePanicking() {
+		panicking=true;
+	}
 
 	public void SetDestination(Vector3 dest) {
 		agent.SetDestination(dest);
@@ -19,22 +28,49 @@ public class NavMeshNavigator : MonoBehaviour
 		agent.speed=speed;
 	}
 
+	private void CheckIfStuck() {
+		if (Vector3.Distance(transform.position, stuckPosition)<stuckDistance) {
+			if (stuckTimer>=timeToConsiderStuck) {
+				stuckTimer=0;
+				stuckPosition=transform.position;
+				GetOppositeDestination();
+			}
+			else stuckTimer+=Time.deltaTime;
+		} else {
+			stuckTimer=0;
+			stuckPosition=transform.position;
+		}
+	}
+
+	private void SetRandomDestination() {
+		agent.SetDestination(targets.GetRandomPoint());
+	}
+
+	private void GetOppositeDestination() {
+		Vector3 dest = agent.destination;
+		//agent.SetDestination(new Vector3(-dest.x, dest.y, -dest.z));
+		agent.SetDestination(transform.position-(transform.forward*3));
+	}
+
     // Start is called before the first frame update
     void Start()
     {
 		agent=GetComponent<NavMeshAgent>();
 		destroyWhenOnDestination=false;
+		stuckTimer=0;
+		timeToConsiderStuck=0.5f;
+		stuckDistance=0.2f;
+		stuckPosition=transform.position;
+		panicking=false;
     }
 
     // Update is called once per frame
     void Update()
     {
 		if (!agent.pathPending) {
-			if (destroyWhenOnDestination&&agent.remainingDistance<=0.4) Destroy(gameObject);
-			else if (agent.remainingDistance<=0.2) {
-				Vector3 destination = targets.GetRandomPoint();
-				agent.SetDestination(destination);
-			}
+			if (destroyWhenOnDestination && agent.remainingDistance<=0.4) Destroy(gameObject);
+			else if (agent.remainingDistance<=0.2) SetRandomDestination();
+			else if (!panicking) CheckIfStuck();
 		}
     }
 }
