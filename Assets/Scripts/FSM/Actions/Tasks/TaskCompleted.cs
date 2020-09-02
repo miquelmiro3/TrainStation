d07e3e1ebb51dfbd43e7a9b9c995dd1b0e1952dd
Samuel <samuel.alarcon@est.fib.upc.edu>
2020-09-02 19:12:38 +0200

    Introduces structure for tasks

---
+++ Assets/Scripts/FSM/Actions/Tasks/TaskCompleted.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "TaskCompleted", menuName = "FSM/Action/Task Completed")]
+public class TaskCompleted : FSMaction
+{
+	public bool value;
+
+	public override void Act(FSMcontroller controller) {
+		TaskHandler.instance.taskDone=value;
+	}
+
+}
