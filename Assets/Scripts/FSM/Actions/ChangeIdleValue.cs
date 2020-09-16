131d8436b1a00de6785518513b7b53afd9eab26f
Sergi Avila Sanguesa <sergi.avila.sanguesa@est.fib.upc.edu>
2020-09-16 11:07:42 +0000

    Fixes the Idle person and adds invisible walls

---
+++ Assets/Scripts/FSM/Actions/ChangeIdleValue.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New ChangeIdleValue", menuName = "FSM/Action/Change Idle Value")]
+public class ChangeIdleValue : FSMaction {
+
+	public bool value;
+
+	public override void Act(FSMcontroller controller) {
+		NavMeshNavigator nmn = controller.GetComponent<NavMeshNavigator>();
+		if (nmn) nmn.idle=value;
+	}
+}
