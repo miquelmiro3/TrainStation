a612af46dfbd36aa171f08d2d089b0632926d271
Sergi Avila Sanguesa <sergi.avila.sanguesa@est.fib.upc.edu>
2020-09-22 17:08:35 +0000

    Adds a red light effect for the alarm

--- Assets/Scripts/StationAlarm.cs
+++ Assets/Scripts/StationAlarm.cs
@@ -6,10 +6,12 @@ public class StationAlarm : MonoBehaviour
 {
 
 	public bool alarmOn;
+	public GameObject lightAnimator;
 	public static StationAlarm instance;
 
 	static public void ActivateAlarm() {
 		instance.alarmOn=true;
+		instance.lightAnimator.GetComponent<Animator>().enabled=true;
 	}
 
 	static public bool GetAlarmState() {
