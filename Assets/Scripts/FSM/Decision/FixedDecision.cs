47355f8aacd3f0fa177d1c39e94bec3f1fa90f79
Samuel <samuel.alarcon@est.fib.upc.edu>
2020-10-05 17:39:57 +0200

    Small changes

--- Assets/Scripts/FSM/Decision/FixedDecision.cs
+++ Assets/Scripts/FSM/Decision/FixedDecision.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 [CreateAssetMenu(fileName = "New FixedDecision", menuName = "FSM/Decision/FixedDecision")]
 public class FixedDecision : FSMdecision {
+
 	public bool fixedValue;
 
 	public override bool Decide(FSMcontroller controller) {
